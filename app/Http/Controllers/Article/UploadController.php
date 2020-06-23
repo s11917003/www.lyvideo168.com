6208e20a7c64699704436cce9d24fff9540a6227
s11917003 <s11917003@gmail.com>
2020-06-23 17:39:31 +0800

    文字至頂

--- app/Http/Controllers/Article/UploadController.php
+++ app/Http/Controllers/Article/UploadController.php
@@ -145,20 +145,20 @@ class UploadController extends Controller {
 						if(config('app.env_machine') == 'win'){ 
 							if($cuttime == 0 && $cuttime2 == 0) {
 								// var_dump(shell_exec("ffprobe -v error -show_entries stream=width,height -of csv=p=0:s=x ".storage_path().'/app'.$path.'/'.$filename.'.m3u8'));
-								shell_exec("ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=Microsoft YaHei Mono.ttf:text='".$watermark."':y=line_h:x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
+								shell_exec("ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=Microsoft YaHei Mono.ttf:text='".$watermark."':x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
 							} else {
 								shell_exec("ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'.mp4'." -ss ".$start." -c copy -t ".$end." ".storage_path().'/app'.$path.'/'.$filename.'1.mp4');
-								shell_exec("ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'1.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=Microsoft YaHei Mono.ttf:text='".$watermark."':y=line_h:x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
+								shell_exec("ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'1.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=Microsoft YaHei Mono.ttf:text='".$watermark."':x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
 								unlink(storage_path().'/app'.$path.'/'.$filename.'1.mp4');
 							}
 						
 						} else {
 							if($cuttime == 0 && $cuttime2 == 0) {
-								shell_exec("/usr/bin/ffmpeg-git-amd64-static/ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=/usr/share/fonts/msyhl.ttc:text='".$watermark."':y=line_h:x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
+								shell_exec("/usr/bin/ffmpeg-git-amd64-static/ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=/usr/share/fonts/msyhl.ttc:text='".$watermark."':x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
 							}
 							else {
 								shell_exec("/usr/bin/ffmpeg-git-amd64-static/ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'.mp4'." -ss ".$start." -c copy -t ".$end." ".storage_path().'/app'.$path.'/'.$filename.'1.mp4');
-								shell_exec("/usr/bin/ffmpeg-git-amd64-static/ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'1.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=/usr/share/fonts/msyhl.ttc:text='".$watermark."':y=line_h:x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
+								shell_exec("/usr/bin/ffmpeg-git-amd64-static/ffmpeg -i ".storage_path().'/app'.$path.'/'.$filename.'1.mp4'." -profile:v baseline -b:v 1200k -maxrate 1200k -b:a 41k -start_number 0 -hls_time 20 -hls_list_size 0 -vf \"drawtext=fontfile=/usr/share/fonts/msyhl.ttc:text='".$watermark."':x=".$Marquee.":fontsize=".$fontsize.":fontcolor=white:shadowx=1:shadowy=1\""." -codec:v libx264 -codec:a copy -y -f hls ".storage_path().'/app'.$path.'/'.$filename.'.m3u8');
 								unlink(storage_path().'/app'.$path.'/'.$filename.'1.mp4');
 							}		
 						}
