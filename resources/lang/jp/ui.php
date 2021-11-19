7eda6cff1ce9cbf8fa48171693542aba79474b8b
s11917003 <s11917003@gmail.com>
2021-11-19 17:45:03 +0800

    切換語系/部分語系文字

--- resources/lang/jp/ui.php
+++ resources/lang/jp/ui.php
@@ -4,4 +4,16 @@ return [
     'tw' => '中国語',
     'en' => '英語',
     'jp' => '日本語',
+    'title' =>[
+        'censored' => '修正あり',
+        'uncensored' => '無修正あり',
+        'amateur' => '素人',
+        'category' => 'ジャンル',
+        'actress' => 'AV女優',
+        'rank' => 'リーダーボード',],
+    'rank' =>[
+        'rank_title' => '人気ランキング',
+        'week' => '週間ランキング',
+        'month' => '月間ランキング'],
+    'search_result' => '検索結果',
 ];
--- resources/lang/tw/ui.php
+++ resources/lang/tw/ui.php
@@ -3,4 +3,17 @@ return [
     'tw' => '中文',
     'jp' => '英文',
     'en' => '日文',
+    'title' =>[
+        'censored' => '有　碼',
+        'uncensored' => '無　碼',
+        'amateur' => '素　人',
+        'category' => '類　別',
+        'actress' => '女　優',
+        'rank' => '排行榜',],
+
+    'rank' =>[
+        'rank_title' => '排行榜一覽',
+        'week' => '每週排行榜',
+        'month' => '每月排行榜'],
+    'search_result' => '搜索結果',
 ];
